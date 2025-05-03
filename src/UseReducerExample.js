5c29c72deb5a1acc19fff963c0ba9716143ceb1f
murttkapln <murttkapln@gmail.com>
2025-05-03 16:14:54 +0300

    feat: useReducer hook used

--- src/UseReducerExample.js
+++ src/UseReducerExample.js
@@ -1,28 +1,34 @@
-import { useState } from "react";
+import { useEffect, useReducer } from "react";
+import { initialState, reducer } from "./reducer";
 
 const UseReducerExample = () => {
-  const [catImage, setCatImage] = useState("");
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  //? destr.
+  const { loading, error, catImage } = state;
 
   const getCatImage = async () => {
     const url = "https://api.thecatapi.com/v1/images/search";
-    setLoading(true);
+
+    dispatch({ type: "START" });
     try {
       const res = await fetch(url);
       const data = await res.json();
-      setCatImage(data[0].url);
-      setError("");
+
+      //? Consuming
+      dispatch({ type: "SUCCESS", payload: data[0].url });
     } catch (error) {
-      setError("DATA CAN NOT BE FETCHED");
-      setCatImage("");
+      dispatch({ type: "FAIL", payload: "DATA CAN NOT BE FETCHED" });
+
       console.log(error);
-    } finally {
-      setLoading(false);
     }
   };
-  console.log(error);
-  console.log(catImage);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     getCatImage();
+  //   }, 5000);
+  //   return () => clearInterval(interval);
+  // }, []);
 
   return (
     <div>
--- src/reducer.js
+++ src/reducer.js
@@ -1,32 +1,35 @@
 export const initialState = {
-  loading: "false",
+  loading: false,
   catImage: "",
   error: "",
-};
+}
 
-export const reducer = ({ initialState, action }) => {
+//? Pure Js fonksiyonu
+export const reducer = (initialState, action) => {
   switch (action.type) {
-    case "START": {
-      return { ...initialState, loading: "true" };
-    }
-    case "SUCCESS": {
+    case "START":
+      return { ...initialState, loading: true }
+    case "SUCCESS":
       return {
         ...initialState,
         catImage: action.payload,
         error: "",
         loading: false,
-      };
-    }
-    case "FAIL": {
+      }
+    case "FAIL":
       return {
         ...initialState,
         catImage: "",
         error: action.payload,
         loading: false,
-      };
-    }
-
+      }
     default:
-      break;
+      break
   }
-};
+}
+
+//? action
+//!  {type: "SUCCESS", payload: catImage }
+//! {type: "START" }
+
+
